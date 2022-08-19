10b35262888ff0bf606afcf1078e6c0fe497ab38
treevesy <mitchell.treeves@student.uts.edu.au>
2022-08-19 16:03:12 +1000

    add .gitignore and home folder

---
+++ home/models.py
@@ -0,0 +1,84 @@
+from django.db import models
+
+from modelcluster.fields import ParentalKey
+
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    MultiFieldPanel,
+    InlinePanel,
+    StreamFieldPanel,
+    PageChooserPanel,
+)
+from wagtail.core.models import Page, Orderable
+from wagtail.core.fields import RichTextField, StreamField
+from wagtail.images.edit_handlers import ImageChooserPanel
+
+from streams import blocks
+
+
+class HomePageCarouselImages(Orderable):
+    """Between 1 and 5 images for the home page carousel."""
+
+    page = ParentalKey("home.HomePage", related_name="carousel_images")
+    carousel_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    panels = [ImageChooserPanel("carousel_image")]
+
+
+class HomePage(Page):
+    """Home page model."""
+
+    template = "home/home_page.html"
+    max_count = 1
+
+    banner_title = models.CharField(max_length=100, blank=False, null=True)
+    banner_subtitle = RichTextField(features=["bold", "italic"])
+    banner_image = models.ForeignKey(
+        "wagtailimages.Image",
+        null=True,
+        blank=False,
+        on_delete=models.SET_NULL,
+        related_name="+",
+    )
+
+    contentCard = StreamField(
+        [
+            ("cards", blocks.CardBlock()),
+        ],
+        null=True,
+        blank=True
+    )
+
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("banner_title"),
+                FieldPanel("banner_subtitle"),
+                ImageChooserPanel("banner_image"),
+
+            ],
+            heading="Banner Options",
+        ),
+        MultiFieldPanel(
+            [InlinePanel("carousel_images", max_num=5, min_num=1, label="Image")],
+            heading="Carousel Images",
+        ),
+        MultiFieldPanel(
+        [
+        StreamFieldPanel("contentCard"),
+        ]
+        ),
+
+    ]
+
+    class Meta:
+
+        verbose_name = "Home Page"
+        verbose_name_plural = "Home Pages"
